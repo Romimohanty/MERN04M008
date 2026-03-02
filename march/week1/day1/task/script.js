c7d931d3ff6d8509f58922a2edef98db257a630a
romi mohanty <romimohantyromimohanty@gmail.com>
2026-03-02 21:08:27 +0530

    array loop

---
+++ march/week1/day1/task/script.js
@@ -0,0 +1,39 @@
+//1.Print numbers from 1 to 100 using a for loop. *
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+
+
+//2.Find the sum of numbers from 1 to 10. *
+let add=0;
+for(let i=1;i<=10;i++){
+    add=add+i;
+}
+
+console.log("sum of 1 to 10",add);
+
+
+
+
+//3.Print multiplication table of 7. *
+let mul=1;
+for(let i=1;i<=10;i++){
+  console.log(7 * i);  
+}
+
+
+
+//4.Find the factorial of a number. *
+let num = 8;
+let fact = 1;
+
+for (let i = 1; i <= num; i++) {
+  fact = fact * i;
+}
+
+console.log(fact);
+
+
+
+//5.Reverse an array using loop. *
